5bff7c96510ae79deda02a5830d0f3857407544a
Rutwik1801 <65919321+Rutwik1801@users.noreply.github.com>
2020-08-15 00:26:32 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+document.querySelector(".js-popup").addEventListener("click",()=>{
+  document.querySelector(".popup").classList.toggle("display");
+    document.querySelector(".triangle").classList.toggle("display");
+});
